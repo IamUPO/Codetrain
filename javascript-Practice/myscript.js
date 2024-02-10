d77dbcceaff2116fdfe67974f604a13be159fc95
PROMISE UZOR OKWUDIRI <iamupoprofessional@gmail.com>
2024-02-10 14:40:11 +0000

    Update

---
+++ javascript-Practice/myscript.js
@@ -0,0 +1,71 @@
+// MyScript.js
+
+// Arithmetic Operators
+let result1 = int1 + int2;         // Addition
+let result2 = float3 * int4;       // Multiplication
+let result3 = float2 / int3;       // Division
+let result4 = int5 - float1;       // Subtraction
+var resultModulus = int5 % int4;   // Modulus
+var preIncrement = ++int1;         // Pre-increment
+var postIncrement = int2++;        // Post-increment
+var preDecrement = --int3;         // Pre-decrement
+var postDecrement = int4--;        // Post-decrement
+
+// Console.log some variables
+console.log("Result of addition:", result1);
+console.log("Result of multiplication:", result2);
+console.log("Result of division:", result3);
+console.log("Result of subtraction:", result4);
+console.log("Result of modulus:", resultModulus);
+console.log("Result of pre-increment:", preIncrement);
+console.log("Result of post-increment:", postIncrement);
+console.log("Result of pre-decrement:", preDecrement);
+console.log("Result of post-decrement:", postDecrement);
+
+///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
+
+// Comparison Operators
+var isEqual = int1 == int2;             // Equal to
+var isIdentical = float1 === string1;   // Identical
+var isNotEqual = string2 != string3;    // Not Equal to
+var isNotIdentical = bool1 !== bool2;    // Not Identical
+var isLessThan = int3 < float2;         // Less than
+var isGreaterThan = float3 > int4;      // Greater than
+var isLessThanOrEqualTo = int5 <= float4;// Less than or equal to
+var isGreaterThanOrEqualTo = float5 >= int1; // Greater than or equal to
+
+// Console.log some variables
+console.log("Is equal:", isEqual);
+console.log("Is identical:", isIdentical);
+console.log("Is not equal:", isNotEqual);
+console.log("Is not identical:", isNotIdentical);
+console.log("Is less than:", isLessThan);
+console.log("Is greater than:", isGreaterThan);
+console.log("Is less than or equal to:", isLessThanOrEqualTo);
+console.log("Is greater than or equal to:", isGreaterThanOrEqualTo);
+
+///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
+
+// Logical Or Boolean Operators
+var andResult = bool1 && bool2;        // AND (&&)
+var orResult = int3 > int2 || bool1;   // OR (||)
+var notResult = !isEqual;               // NOT (!)
+
+// Console.log some variables
+console.log("AND result:", andResult);
+console.log("OR result:", orResult);
+console.log("NOT result:", notResult);
+
+///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
+
+// String Operators
+var concatenatedString = string1 + " " + string2 + " " + string3 + " " + string4 + " " + string5;    // Concatenation using +
+
+// Console.log the concatenated string
+console.log("Concatenated String:", concatenatedString);
