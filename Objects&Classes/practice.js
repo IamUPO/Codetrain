1fdd4694098a6a14d8b9187f17666797005d442d
PROMISE UZOR OKWUDIRI <iamupoprofessional@gmail.com>
2024-03-02 17:50:28 +0000

    Update

--- Objects&Classes/practice.js
+++ Objects&Classes/practice.js
@@ -14,3 +14,22 @@ const myFav = {
 
 console.log(Object.keys(mySelf), Object.values(mySelf));
 console.log(Object.keys(myFav), Object.values(myFav));
+
+
+class Human {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    talk() {
+        console.log(this.name + " speaks");
+    }
+}
+
+class Man extends Human {
+}
+
+const john = new Man("John", 30);
+
+john.talk();
