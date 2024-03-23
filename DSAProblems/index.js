bbd7310102bef35c2e7c3f66dc4fb733b7b79300
PROMISE UZOR OKWUDIRI <iamupoprofessional@gmail.com>
2024-03-23 13:36:52 +0000

    update

--- DSAProblems/index.js
+++ DSAProblems/index.js
@@ -34,5 +34,5 @@ const studentData = [
 //     console.log(student);
 // }
 
-const gen24Student = studentData.filter((studetData) => studetData.gen === 24);
+const gen24Student = studentData.filter((studetData) => studetData.gen === 28);
 console.log(gen24Student);
