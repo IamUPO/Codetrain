3519d13c100e5736cbb955c56a00e467b25fc31f
PROMISE UZOR OKWUDIRI <iamupoprofessional@gmail.com>
2024-03-23 12:42:44 +0000

    Update

---
+++ DSAProblems/index.js
@@ -0,0 +1,8 @@
+var name = "promise";
+
+let correctName;
+
+// correctName = name.charAt(0).toUpperCase() + name.slice(1);
+// correctName = name[0].toUpperCase() + name.slice(1);
+
+console.log(correctName);
