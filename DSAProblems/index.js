eb31fca8ca2de0b0d53a161730b95a24a45c3eef
PROMISE UZOR OKWUDIRI <iamupoprofessional@gmail.com>
2024-03-23 13:35:22 +0000

    Update index.js

--- DSAProblems/index.js
+++ DSAProblems/index.js
@@ -1,8 +1,38 @@
-var name = "promise";
+// var name = "promise";
 
-let correctName;
+// let correctName;
 
-// correctName = name.charAt(0).toUpperCase() + name.slice(1);
-// correctName = name[0].toUpperCase() + name.slice(1);
+// // correctName = name.charAt(0).toUpperCase() + name.slice(1);
+// // correctName = name[0].toUpperCase(){ + name.slice(1);
+// // correctName = `${name[0].toUpperCase()} ${name.slice(1)}`;
 
-console.log(correctName);
+// console.log(correctName);
+
+// for (i = 0; i < 5; i++) {
+//     console.log("Hello World");
+// }
+
+// let array = [];
+
+// Array(5).fill("Hello World").forEach(message => console.log(message));
+
+const studentData = [
+    { name: "Kwabc", email: "kwabc@gmail.com", gen: 25, },
+    { name: "Promise", email: "promise@gmail.com", gen: 27, },
+    { name: "Ngozi", email: "ngozi@gmail.com", gen: 25, },
+    { name: "Sam", email: "sam@gmail.com", gen: 27, },
+    { name: "Robert", email: "robert@gmail.com", gen: 24, },
+];
+
+// console.table(studentData);
+
+// for (let i = 0; i < studentData.length; i++) {
+//     console.log(studentData[i]);
+// }
+
+// for (let student of studentData) {
+//     console.log(student);
+// }
+
+const gen24Student = studentData.filter((studetData) => studetData.gen === 24);
+console.log(gen24Student);
