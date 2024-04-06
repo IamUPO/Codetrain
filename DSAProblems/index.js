78414e2d84c43b0e6f500a14c7bff47f3b2a9c5e
PROMISE UZOR OKWUDIRI <iamupoprofessional@gmail.com>
2024-04-06 12:26:28 +0000

    Refactor DSAProblems/index.js file

--- DSAProblems/index.js
+++ DSAProblems/index.js
@@ -16,13 +16,13 @@
 
 // Array(5).fill("Hello World").forEach(message => console.log(message));
 
-const studentData = [
-    { name: "Kwabc", email: "kwabc@gmail.com", gen: 25, },
-    { name: "Promise", email: "promise@gmail.com", gen: 27, },
-    { name: "Ngozi", email: "ngozi@gmail.com", gen: 25, },
-    { name: "Sam", email: "sam@gmail.com", gen: 27, },
-    { name: "Robert", email: "robert@gmail.com", gen: 24, },
-];
+// const studentData = [
+//     { name: "Kwabc", email: "kwabc@gmail.com", gen: 25, },
+//     { name: "Promise", email: "promise@gmail.com", gen: 27, },
+//     { name: "Ngozi", email: "ngozi@gmail.com", gen: 25, },
+//     { name: "Sam", email: "sam@gmail.com", gen: 27, },
+//     { name: "Robert", email: "robert@gmail.com", gen: 24, },
+// ];
 
 // console.table(studentData);
 
@@ -34,5 +34,62 @@ const studentData = [
 //     console.log(student);
 // }
 
-const gen24Student = studentData.filter((studetData) => studetData.gen === 27);
-console.log(gen24Student);
+// const gen24Student = studentData.filter((studetData) => studetData.gen === 27);
+// console.log(gen24Student);
+
+// DSA Class Two (Destructuring)
+
+// let student = {
+//     name: "Promise",
+//     email: "Promise@gmail.com",
+//     gen: 27,
+//     age: 25,
+// }
+
+// const studentName = student.name;
+// // console.log(studentName)
+
+// let {name, age: currentName=1, gen, email , phoneNumber= "0542939165"} = student;
+
+// console.log(currentName);
+
+//I have two CSSSupportsRule, and on my left Cup is salt and on the right Cup is Sugar, How do I swap the sugar for the salt cup.
+// let leftCup = "salt";
+// let rightCup = "sugar";
+
+// let temp = leftCup;
+// leftCup = rightCup;
+// rightCup = temp;
+
+// [leftCup="salt", rightCup="sugar"] = [rightCup="sugar", leftCu="salt"];
+
+// console.log("Left Cup: " + leftCup);
+// console.log("Right Cup: " + rightCup);
+
+
+// let name = ['will', 'promise', 'kwabc', 'sam', 'ngozi', 'robert'];
+
+// [a,b, ...rest] = name;
+
+// console.log(a);
+
+// Create a nested object and destructure it
+const nestedObject = {
+    property1: {
+        subProperty1: "value1",
+        subProperty2: "value2",
+    },
+    property2: {
+        subProperty3: "value3",
+        subProperty4: "value4",
+    },
+};
+
+const { property1, property2 } = nestedObject;
+const { subProperty1, subProperty2 } = property1;
+const { subProperty3, subProperty4 } = property2;
+
+console.log(subProperty1);
+console.log(subProperty2);
+console.log(subProperty3);
+console.log(subProperty4);
