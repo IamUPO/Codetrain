fd888150df8e6df67f35f9733f5b84f561a5969a
PROMISE UZOR OKWUDIRI <158743974+iamupo@users.noreply.github.com>
2024-05-04 13:53:00 +0000

    Update

--- ReactFuctions/src/components/button.jsx
+++ ReactFuctions/src/components/button.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
 
-// function Button(props) {
+function Button(props) {
 
-//     return (
-//         <div>
-//             <button>{props.name}</button>
-//         </div>
-//     )
-// }
-
-class Button extends React.PureComponent{
-    render(){
-        return (
-            <div>
-                <button style={{backgroundColor: this.props.buttonColor, color: this.props.textColor }}>{this.props.name}</button>
-            </div>
-        )
-    }
+    return (
+        <div>
+            <button>{props.name}</button>
+        </div>
+    )
 }
+
+// class Button extends React.PureComponent{
+//     render(){
+//         return (
+//             <div>
+//                 <button style={{backgroundColor: this.props.buttonColor, color: this.props.textColor }}>{this.props.name}</button>
+//             </div>
+//         )
+//     }
+// }
 
 export default Button
