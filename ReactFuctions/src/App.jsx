05510cd1719eb295931d52300734a63f642176b8
PROMISE UZOR OKWUDIRI <158743974+iamupo@users.noreply.github.com>
2024-05-04 11:59:37 +0000

    update

---
+++ ReactFuctions/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './styles/index.css'
+
+// function App() {
+//   return (
+//     <>
+//       <h1>I love coding</h1>
+//     </>
+//   )
+// }
+
+// export default App
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>I love coding</h1>
+      </>
+    )
+  }
+}
+
+export default App
