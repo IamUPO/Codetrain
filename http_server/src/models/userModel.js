1b3a170ec08e9b8f779c7fcd88afc816273d43e4
Promise Uzor <uzorpromiseokwudiri@gmail.com>
2024-11-26 13:26:06 +0000

    update

---
+++ http_server/src/models/userModel.js
@@ -0,0 +1,51 @@
+import express from 'express';
+
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    avatar: {
+      type: String,
+      default: "",
+      trim: true, 
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true, 
+    },
+    email: {
+      type: String,
+      required: false, 
+      unique: true, 
+      lowercase: true, 
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: false, 
+      minlength: 6,
+    },
+    bio: {
+      type: String,
+      default: "",
+      trim: true, 
+      maxlength: 160,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
