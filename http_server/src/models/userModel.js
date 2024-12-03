e09759e578864dd7db1e9118f851fca15143b1cb
Promise Uzor <uzorpromiseokwudiri@gmail.com>
2024-12-03 14:31:56 +0000

    "Added 'lastLgin' field to userSchema with default value of current date and time."

--- http_server/src/models/userModel.js
+++ http_server/src/models/userModel.js
@@ -42,6 +42,11 @@ const userSchema = new mongoose.Schema(
       type: Date,
       default: Date.now,
     },
+    lastLgin: {
+      type: Date,
+      default: Date.now,
+    },
+    
   },
   { timestamps: true }
 );
